db66aa7478d362f99a475fb34ba10c13861db8d0
MokhammadVu <stechkin_95@hotmail.com>
2024-10-31 15:33:24 +0100

    up

--- src/app/components/slogan/Slogan.tsx
+++ src/app/components/slogan/Slogan.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './styles.module.css';
 
 const SloganSection = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const companyName = document.querySelector(`.${styles.companyName}`);
+      const slogan = document.querySelector(`.${styles.slogan}`);
+      const seperator = document.querySelector(`.${styles.seperator}`);
+
+      // Проверяем, что элементы не равны null
+      if (companyName && slogan) {
+        const companyPosition = companyName.getBoundingClientRect().top;
+        const sloganPosition = slogan.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.2;
+
+        if (companyPosition < screenPosition) {
+          companyName.classList.add(styles.visible);
+        }
+        if (sloganPosition < screenPosition) {
+          slogan.classList.add(styles.visible);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <section className={styles.sloganSection}>
       <div className={styles.sloganContainer}>
