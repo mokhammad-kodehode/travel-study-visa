07cff4f2fb792f62b8094f8572afa01b5910f17f
MokhammadVu <stechkin_95@hotmail.com>
2024-11-01 13:46:17 +0100

    up

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.travelandstudy.ru", // Укажите ваш сайт
+  generateRobotsTxt: true, // Генерировать robots.txt (опционально)
+  sitemapSize: 7000, // Максимальный размер sitemap
+};
