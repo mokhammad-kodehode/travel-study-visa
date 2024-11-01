311de6686e73156b33ccb5510422a58a52fa2e6d
MokhammadVu <stechkin_95@hotmail.com>
2024-11-01 13:13:01 +0100

    up

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.travelandstudy.ru",
-  generateRobotsTxt: true, // Генерация robots.txt
-  sitemapSize: 7000, // Максимальное количество URL в одном файле sitemap
-};
